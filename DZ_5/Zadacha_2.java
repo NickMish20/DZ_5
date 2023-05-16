409bf9ab9bbe18e7fb8fa9f1c0c1dcad5130d023
NickMish <nickmish@mail.ru>
2023-05-16 23:47:35 +0300

    Задача 2

---
+++ DZ_5/Zadacha_2.java
@@ -0,0 +1,55 @@
+package DZ_5;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class Zadacha_2 {
+    // 2) Реализовать алгоритм пирамидальной сортировки (HeapSort).
+    public static void main(String[] args) {
+        int[] arr = new int[20];
+        Random rnd = new Random();
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = rnd.nextInt(40);
+        }
+        System.out.println(Arrays.toString(arr));
+        heapSort(arr);
+        System.out.println(Arrays.toString(arr));
+        System.out.println("Сам решил \"Отвечаю\" :)");
+    }
+
+    public  static void heapSort(int arr[]) {
+        int n = arr.length;
+        for (int i = n / 2 - 1; i >= 0; i--)
+            heapify(arr, n, i);
+
+        for (int i = n - 1; i >= 0; i--) {
+            int temp = arr[0];
+            arr[0] = arr[i];
+            arr[i] = temp;
+
+            heapify(arr, i, 0);
+        }
+    }
+
+    public  static void heapify(int arr[], int n, int i) {
+        int largest = i; 
+        int l = 2 * i + 1; 
+        int r = 2 * i + 2; 
+
+        if (l < n && arr[l] > arr[largest])
+            largest = l;
+
+        if (r < n && arr[r] > arr[largest])
+            largest = r;
+
+        if (largest != i) {
+            int swap = arr[i];
+            arr[i] = arr[largest];
+            arr[largest] = swap;
+
+            heapify(arr, n, largest);
+        }
+    }
+
+}
+
